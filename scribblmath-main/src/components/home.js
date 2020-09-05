318f9101a094f5b42bd37c0ee3e47e5ebed7a637
Justin Kyle <justinklchang@yahoo.com>
2020-09-05 10:36:27 -0700

    Added router functionality

---
+++ scribblmath-main/src/components/home.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import createClass from 'create-react-class';
+import {Link} from 'react-router-dom';
+import './home.css';
+
+// get our fontawesome imports
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPencilRuler, faSortDown } from '@fortawesome/free-solid-svg-icons'
+
+var Home = createClass ({
+render: function () {
+    return (
+      <div className="App">
+        <header className="App-header">  
+          <FontAwesomeIcon className="App-logo" icon={faPencilRuler} />
+          <h1 className = "App-title">
+            Scribble Math
+          </h1>
+          <Link to ="/login"><button className = "login-button" type ="button">
+            Click here to log in
+          </button></Link>
+
+          <div className = "create-account-option">Not a member? <a href="#">Create an account</a> </div>
+          <br></br>
+          <FontAwesomeIcon className="arrow-scroll" icon={faSortDown}/>
+        </header>
+
+
+        <section className="Description-section">
+          <h2>Our Goal</h2>
+          <br></br>
+          <br></br>
+          <br></br>
+        </section>
+      </div>
+    );
+  }
+});
+
+export default Home;
