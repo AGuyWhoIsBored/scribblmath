d7869d9d23881bf452fd3c2dd65cbcd2a09c8fcb
Justin Kyle <justinklchang@yahoo.com>
2020-09-05 16:59:42 -0700

    Added cancel for login and signup

--- scribblmath-main/src/components/createacc.js
+++ scribblmath-main/src/components/createacc.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import createClass from 'create-react-class';
+import {Link} from 'react-router-dom';
 import './createacc.css';
+
+// get our fontawesome imports
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 var createacc = createClass ({
   render: function () {
@@ -8,14 +13,18 @@ var createacc = createClass ({
       <div className = "signup-header">
         <form action="/signup" method="POST">
           <div className="form-contents">
+            <div class = "cancel-icon-positioning-2">
+                    <Link to="/">
+                      <FontAwesomeIcon icon={faTimes} className = "cancel-icon"></FontAwesomeIcon>
+                    </Link>
+            </div>
             <div className="spacing">
-              <div className="signup-title black-font">Create your account</div>
-
+                <div className="signup-title black-font">Create your account</div>
+                
                 <div><input type="text" placeholder="Enter username" name="name" required></input></div>
                 <div><input type="email" placeholder="Enter email" name="email" required></input></div>
                 <div><input type="password" placeholder="Enter password" name="password" required></input></div>
                 <div><input type="password" placeholder="Re-enter password" name="password" required></input></div>
-              
 
                 <div>
                 <button type="submit" className="signup-button">Submit</button>
--- scribblmath-main/src/components/home.js
+++ scribblmath-main/src/components/home.js
@@ -15,7 +15,7 @@ render: function () {
         <header className="App-header">  
           <FontAwesomeIcon className="App-logo" icon={faPencilRuler} />
           <h1 className ="App-title">
-            Scribbl Math
+            ScribblMath
           </h1>
 
           <p>Easily convey complex mathematical ideas in a virtual setting!</p>
@@ -27,13 +27,26 @@ render: function () {
           <div className = "create-account-option">Not a member? <Link to = "/signup" style={{ color: "blue"}}>Create an account</Link> </div>
           <br></br>
 
-          <ScrollLink activeClass="active" to="Description-section" spy={true} smooth={true} offset={-70} duration={500}><FontAwesomeIcon className="arrow-scroll" icon={faSortDown}/></ScrollLink>
+          <ScrollLink to="Description-section" spy={true} smooth={true} offset={-70} duration={500}><FontAwesomeIcon className="arrow-scroll" icon={faSortDown}/></ScrollLink>
         </header>
 
 
         <section className="Description-section">
-          <h2>Our Goal</h2>
-          <p></p>
+          <h1 style={{fontSize: "30px"}}><b>Our Goal</b></h1>
+          <div>
+            <img src="http://loremflickr.com/420/400/abstract" id = "Description-image"></img>
+            <div id = "Description-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but
+              also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+              containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but
+              also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+              containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </div>
+            
+          </div>
         </section>
       </div>
     );
