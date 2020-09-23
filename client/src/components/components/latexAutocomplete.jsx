61691c829d5fddc3fe7ac04c0a097f08027c6588
Justin Kyle Chang <justinklchang@yahoo.com>
2020-09-23 13:31:13 -0700

    Added list of potentially working latex commands

--- client/src/components/components/latexAutocomplete.jsx
+++ client/src/components/components/latexAutocomplete.jsx
@@ -1,24 +1,62 @@
 import React, { Component } from 'react';
 import PropTypes, { string } from 'prop-types';
  
-//Current list of Latex Commands
+//Current list of Latex Commands (https://www.ntg.nl/doc/biemesderfer/ltxcrib.pdf)
 var registeredLatexCommands = [
-  "Dog",
-  "Cat",
-  "Woof",
-  "Go",
-  "Papaya",
-  "Meow",
-  "Persimmon",
-  "PawPaw",
-  "Prickly",
-  "Peach",
-  "Pomegranate",
-  "Pineapple",
-  'Pear',
-  "Pipe",
-  "Pack",
-  "Puddle"
+  //first page of pdf (filtered)
+  '\\#', '\\$', '\\%', '\\&', '\\*', '\\,', '\\.', '\\/', '\\:', '\\;', '\\@', '\\[', 
+  '\\]', '\\^', '\\_', '\\{', '\\|', '\\}', '\\~', "\\a’", "\\a‘", '\\a=', '\\aa', '\\acute', 
+  '\\ae', '\\AE', '\\aleph', '\\alpha', '\\amalg', '\\angle', '\\approx', '\\arccos', '\\arcsin', 
+  //second page of pdf (filtered)  
+  '\\arctan', '\\arg', '\\ast', '\\asymp', '\\b', '\\backlash', '\\bar',
+  //third page of pdf (filtered)
+  '\\beta', '\\bf', '\\bigcap', '\\bigcirc', '\\bigcup', '\\bigodot', '\\bigoplus', '\\bigotimes', 
+  '\\bigtriangledown', '\\bigtriangleup', '\\bigsqcup', '\\biguplus', '\\bigvee', '\\bigwedge', '\\bmod',
+  '\\bot', '\\bowtie', '\\Box', '\\breve', '\\bullet', '\\c', '\\cal', '\\cap', '\\cdot', '\\cdots', 
+  '\\check', '\\chi', '\\circ', '\\clubsuit', '\\cong', '\\coprod',
+  //fourth page of pdf (filtered)  
+  '\\copyright', '\\cos', '\\cosh', '\\cot', '\\coth', '\\csc', '\\cup', '\\d', '\\dag', '\\dagger',
+  '\\dashv', '\\ddag', '\\ddagger', '\\ddot','\\ddots', '\\deg','\\delta', '\\det', '\\diamond', 
+  '\\Diamond', '\\diamondsuit', '\\dim', '\\displaystyle', '\\div', '\\dot', '\\doteq', '\\downarrow', 
+  '\\Downarrow', '\\ell', '\\em', '\\emptyset', '\\epsilon', '\\equiv', '\\eta', '\\exists', '\\exp',
+  '\\fbox{text}', '\\flat',
+  //fifth page of pdf (filtered)
+  '\\forall', '\\frac{numerator}{denominator}', '\\frown', '\\gamma', '\\Gamma', '\\gcd', '\\ge', 
+  '\\geq', '\\gets', '\\gg', '\\grave', '\\H', '\\hat', '\\hbar', '\\heartsuit', '\\hom', '\\hookleftarrow',
+  '\\hookrightarrow', '\\huge', '\\Huge', '\\i', '\\iff', '\\Im', '\\imath', '\\in',
+  '\\inf', '\\infty', '\\int', '\\iota', '\\it',
+  //sixth page of pdf (filtered)
+  '\\j', '\\jmath', '\\Join', '\\kappa', '\\ker', '\\l', '\\L', '\\lambda', '\\Lambda', '\\land',
+  '\\langle', '\\large', '\\Large', '\\LARGE', '\\LaTeX', '\\lbrace', '\\lbrack', '\\lceil', '\\ldots',
+  '\\le', '\\leadsto', '\\leftarrow', '\\Leftarrow', '\\leftharpoondown', '\\leftharpoonup', '\\leftrightarrow', 
+  '\\Leftrightarrow', '\\leq', '\\lfloor', '\\lg', '\\lhd', '\\lim', '\\liminf', '\\limsup', '\\ll', '\\ln', 
+  '\\lnot', '\\log', '\\longleftarrow', '\\Longleftarrow', '\\longleftrightarrow', '\\Longleftrightarrow', 
+  '\\longmapsto', '\\longrightarrow', '\\Longrightarrow', '\\lor', '\\lq', 
+  //seventh page of pdf (filtered)
+  '\\mapsto', '\\max', '\\mbox{text}', '\\mho', '\\mid', '\\min', '\\mit', '\\models', '\\mp', 
+  '\\mu', '\\nabla', '\\natural', '\\new', '\\nearrow', '\\neg', '\\neq', '\\ni', '\\not',
+  '\\notin', '\\nu', '\\nwarrow', '\\o', '\\O',
+  //eighth page of pdf (filtered)
+  '\\odot', '\\oe', '\\OE', '\\oint', '\\omega', '\\Omega', '\\ominus',
+  '\\oplus', '\\oslash', '\\otimes', '\\overbrace{text}', '\\overline{text}', '\\owns',
+  '\\P', '\\parallel', '\\partial', '\\perp', '\\phi', '\\pi', '\\PI', '\\pm', '\\pmod{modulus}', 
+  '\\pounds', '\\Pr', '\\prec', '\\preceq', '\\prime', '\\prod', '\\propto', '\\psi', '\\Psi', 
+  //ninth page of pdf (filtered)
+  '\\rangle', '\\rbrace', '\\rbrack', '\\rceil', '\\Re', '\\rfloor', '\\rhd', '\\rho', '\\rightarrow',
+  '\\Rightarrow', '\\rightharpoondown', '\\rightharpoonup', '\\rightleftharpoons', '\\rm', '\\rq', 
+  '\\S', '\\sc', '\\searrow', '\\sec', '\\setminus', '\\sf', '\\sharp', '\\sigma', '\\Sigma', 
+  '\\sim', '\\simeq', '\\sin', '\\sinh',
+  //tenth page of pdf (filtered)
+  '\\small', '\\smallint', '\\smile', '\\spadesuit', '\\sqcap', 'sqcup', '\\sqrt[#]{arg}', '\\sqsubset', 
+  '\\sqsubseteq', '\\sqsupset', '\\sqsupseteq', '\\ss', '\\star', '\\subset', '\\subseteq', '\\succ', '\\succeq',
+  '\\sum', '\\sup', '\\supset', '\\supseteq', '\\surd', '\\swarrow', '\\symbol{cc}', 
+  '\\t', '\\tan', '\\tanh', '\\tau', '\\TeX', '\\theta', '\\Theta', '\\tilde', '\\times', '\\tiny', '\\to',
+  //eleventh page of pdf (filtered)
+  '\\top', '\\triangle', '\\trianglefeft', '\\triangleright', '\\u', '\\unboldmath', '\\underbrace{text}',
+  '\\underline{text}', '\\unlhd', '\\unrhd', '\\uparrrow', '\\Uparrow', '\\updownarrow', '\\Updownarrow', '\\uplus', 
+  '\\upsilon', '\\Upsilon', '\\v', '\\varepsilon', '\\varphi', '\\varpi', '\\varrho', '\\varsigma', '\\vartheta', 
+  '\\vdash', '\\vdots','\\vec', '\\vee', '\\vert', '\\Vert', '\\wedge', '\\widehat{arg}', '\\widetilde{arg}', '\\wp', 
+  '\\wr', '\\xi', '\\Xi', '\\zeta'
 ];
  
 export default class Autocomplete extends Component {
@@ -31,7 +69,8 @@ export default class Autocomplete extends Component {
   state = {
       wrapperPresent: '',
       charStart: 0,
-      activeOption: 0,
+      activeOption: -1,
+      charCount: 0, 
       filteredOptions: [],
       showOptions: false,
       autocompleteStatus: false,
@@ -42,7 +81,7 @@ export default class Autocomplete extends Component {
   /*When content in input box changes */
   onChange = (e) => {
     
-    const { charStart, prevInput, wrapperPresent} = this.state;
+    const { charStart, prevInput, wrapperPresent, charCount} = this.state;
     const { wrapper } = this.props;
 
     console.log("Our wrapper: " + wrapper);
@@ -53,7 +92,8 @@ export default class Autocomplete extends Component {
       this.setState({
         wrapperPresent: '',
         charStart: 0,
-        activeOption: 0,
+        charCount: 0, 
+        activeOption: -1,
         filteredOptions: [],
         showOptions: false,
         autocompleteStatus: false,
@@ -119,7 +159,10 @@ export default class Autocomplete extends Component {
         }
         //if wrapperCount after deletion is even, deactivate wrapper state
         else {
-          this.setState({wrapperPresent: ''})
+          this.setState({
+            wrapperPresent: '',
+            showOptions: false  
+          })
         }
       }
     }
@@ -142,7 +185,7 @@ export default class Autocomplete extends Component {
       }
  
       //Deals with closing wrapper ('$')
-      if (userInputTemp.indexOf(wrapper) === userInputTemp.length - 1) {
+      if (userInputTemp.indexOf(wrapper) === userInputTemp.length - 1 && charCount <= currentVal.length) {
         this.setState({
           prevInput: prevInput + userInputTemp,
           charStart: charStart + userInputTemp.length,
@@ -189,7 +232,8 @@ export default class Autocomplete extends Component {
     }
  
     this.setState({
-      activeOption: 0,
+      activeOption: -1,
+      charCount: currentVal.length,
       autocompleteStatus: false,
       userInput: currentVal
     });
@@ -198,7 +242,7 @@ export default class Autocomplete extends Component {
   onClick = (e) => {
     const { prevInput } = this.state;
     this.setState({
-      activeOption: 0,
+      activeOption: -1,
       filteredOptions: [],
       showOptions: false,
       userInput: prevInput + e.currentTarget.innerText
@@ -208,11 +252,12 @@ export default class Autocomplete extends Component {
   onKeyDown = (e) => {
     const { activeOption, filteredOptions, prevInput, autocompleteStatus } = this.state;
  
+    //enter key
     if (e.keyCode === 13) {
       console.log("THIS RAN FIRST")
-      if (document.querySelector('ul.options')){        
+      if (document.querySelector('ul.options') && activeOption !== -1){        
         this.setState({
-          activeOption: 0,
+          activeOption: -1,
           showOptions: false,
           userInput: prevInput + filteredOptions[activeOption],
           autocompleteStatus: true
@@ -224,18 +269,36 @@ export default class Autocomplete extends Component {
         this.setState({autocompleteStatus: false})
       }
     }
+    //up arrow key
     else if (e.keyCode === 38) {
-      if (activeOption === 0) {
+      if (activeOption === -1) {
         return;
       }
       this.setState({ activeOption: activeOption - 1 });
+      if (activeOption - 1 === -1)
+        this.setState({showOptions: false})
     } 
+    //down arrow key
     else if (e.keyCode === 40) {
       if (activeOption === filteredOptions.length - 1) {
         console.log(activeOption);
         return;
       }
       this.setState({ activeOption: activeOption + 1 });
+    }
+    //left arrow key
+    else if (e.keyCode === 37) {
+      this.setState({
+        showOptions: false,
+        activeOption: -1
+      })
+    }
+    //right arrow key
+    else if (e.keyCode === 39) { 
+      this.setState({
+        showOptions: false,
+        activeOption: -1
+      })
     }
   };
  
